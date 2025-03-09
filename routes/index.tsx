d287ecd148933effb72b7643dcf3bacd65bef1a5
Seth Lang <sethwlang@proton.me>
2025-03-08 21:53:58 -0500

    Fresh update

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,29 +1,32 @@
 export default function Home() {
   return (
       <main>
-        <img src="/path/to/your/image.jpg"
-            alt="An image of Bailey"
-            class="float-image"
-        />
-        <h1>Magic happens at The Writing Desk.</h1>
-        <p>Pull up a comfy chair and grab your fanciest notebook. It’s time to write. </p>
-        <h2>Let’s tell a story that matters. </h2>
-        <p>Your story.</p>
-        <p>You know the one: the book you’ve been waiting to start until you have more free time, or inspiration
-          strikes, or you finally feel ready.
-        </p>
-        <p>I’ll let you in on a secret. The free time isn’t coming, the Muse is waiting on you, and no writer is ever
-          truly ready. The real magic is knowing all that and figuring out how to write anyway. The best way to tap into
-          that magic?
-        </p>
-        <p>Sitting down at The Writing Desk, of course.</p>
-        <p>You deserve a writing practice that meets your needs and supports your best work. A writing practice that
-          fuels your creativity and makes writing your book—dare I say it?—fun. </p>
-        <p>At the Writing Desk, we’ll build that practice together. </p>
-        <p>Working with a book coach opens the portal to a deeply personalized, sustainable writing practice. Together,
-          we’ll take your book from draft to done. </p>
-        <a href="/coaching">Learn About Book Coaching</a>
-        <h3> Newsletter Signup Form</h3>
+
+          <h1>Magic happens at The Writing Desk.</h1>
+          <p>Pull up a comfy chair and grab your fanciest notebook. It’s time to write. </p>
+          <h2>Let’s tell a story that matters. </h2>
+          <img src="/images/LandingPage.webp"
+               alt="An image of Bailey"
+               className="float-image"
+          />
+          <p>Your story.</p>
+          <p>You know the one: the book you’ve been waiting to start until you have more free time, or inspiration
+              strikes, or you finally feel ready.
+          </p>
+          <p>I’ll let you in on a secret. The free time isn’t coming, the Muse is waiting on you, and no writer is ever
+              truly ready. The real magic is knowing all that and figuring out how to write anyway. The best way to tap
+              into
+              that magic?
+          </p>
+          <p>Sitting down at The Writing Desk, of course.</p>
+          <p>You deserve a writing practice that meets your needs and supports your best work. A writing practice that
+              fuels your creativity and makes writing your book—dare I say it?—fun. </p>
+          <p>At the Writing Desk, we’ll build that practice together. </p>
+          <p>Working with a book coach opens the portal to a deeply personalized, sustainable writing practice.
+              Together,
+              we’ll take your book from draft to done. </p>
+          <a href="/coaching">Learn About Book Coaching</a>
+          <h3> Newsletter Signup Form</h3>
       </main>
   );
 }
